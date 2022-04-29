4d761b052a9f5a47e2aca860fadfc4688aa26006
guzicco <zawilski.michaal@gmail.com>
2022-04-29 13:25:04 +0200

    Fetch Directory Data minimal css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { API_LINK } from "./Globals";
+import styled from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currentDir, setCurrentDir] = useState<IDir | undefined>(undefined);
+
+  interface IDir {
+    name: string;
+    id: string;
+    files?: [{ name: string }];
+    directories?: IDir[];
+  }
+
+  const fetchDirData = async (id = "") => {
+    setLoading(true);
+    try {
+      const dirData = await axios
+        .get(API_LINK + id)
+        .then((response) => response.data);
+      setCurrentDir(dirData);
+    } catch (err) {
+      console.error(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDirData();
+  }, []);
+
+  const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  return (
+    <div className="App">
+      <Content>
+        {currentDir?.directories?.map((dir) => (
+          <p>{dir.name}</p>
+        ))}
+      </Content>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Globals.tsx
@@ -0,0 +1,1 @@
+export const API_LINK = `https://fnp5vd20r2.execute-api.us-east-1.amazonaws.com/dev/directories/`;
