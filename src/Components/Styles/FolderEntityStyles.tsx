4081bee433a5ff86a660d6d8b5ef082f59757477
guzicco <zawilski.michaal@gmail.com>
2022-05-01 16:55:25 +0200

    folder entity background change on hover

--- src/Components/Styles/FolderEntityStyles.tsx
+++ src/Components/Styles/FolderEntityStyles.tsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 const FolderEntityStyles = styled.div`
   width: 100px;
   padding: 20px;
-  border: solid 1px red;
   cursor: pointer;
+  border-radius: 20px;
+  transition: background-color 0.33s ease;
+
+  :hover {
+    background-color: rgba(200, 200, 200, 0.5);
+  }
 `;
 
 export default FolderEntityStyles;
