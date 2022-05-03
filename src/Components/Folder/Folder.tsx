8bdde760aaff42dff1392880163451fb5839f4f4
guzicco <zawilski.michaal@gmail.com>
2022-05-03 20:21:19 +0200

    moved Folder and icons to seperate folder

---
+++ src/Components/Folder/Folder.tsx
@@ -0,0 +1,18 @@
+import { IDir, IPath } from "../../types";
+import FolderEntity from "../../StyledComponents/FolderEntity";
+const FolderIcon = require("./Folder.png");
+
+interface IProps extends IDir {
+  onClick: (path: IPath) => void;
+}
+
+const Folder: React.FC<IProps> = ({ name, id, onClick }) => {
+  return (
+    <FolderEntity onClick={() => onClick({ name, id })}>
+      <img src={FolderIcon} alt="Folder" />
+      <p>{name}</p>
+    </FolderEntity>
+  );
+};
+
+export default Folder;
