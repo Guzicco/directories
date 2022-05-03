13a7ff5f5c75094be62ca5220193d66b8d1c6ed9
guzicco <zawilski.michaal@gmail.com>
2022-05-03 21:19:27 +0200

    changed FolderEntity into button tag enables for tab&enter movement around folders

--- src/StyledComponents/FolderEntity.tsx
+++ src/StyledComponents/FolderEntity.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
-const FolderEntity = styled.div`
+const FolderEntity = styled.button`
   width: 100px;
   padding: 20px;
   cursor: pointer;
+  border: none;
   border-radius: 20px;
+  background-color: inherit;
+  color: white;
   transition: background-color 0.33s ease;
 
-  :hover {
+  :hover,
+  :focus {
     background-color: rgba(232, 234, 237, 0.04);
+    outline: none;
   }
 `;
 
